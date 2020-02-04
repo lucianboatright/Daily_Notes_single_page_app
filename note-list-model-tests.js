1f9f1c4b7dc18c9d3528c0e00905c543558e6173
Asia <asiaellis5@gmail.com>
2020-02-04 14:57:15 +0000

    moved repositories

---
+++ note-list-model-tests.js
@@ -0,0 +1,24 @@
+(function testNoteList() {
+  var noteList = new NoteList()
+  assert.isTrue(noteList.notes.length === 0);
+  console.log('testNoteList passing')
+})();
+
+(function testAddNoteList() {
+  var notelist = new NoteList()
+  notelist.add('hello')
+  notelist.add('test')
+  assert.isTrue(notelist.notes.length === 2)
+  assert.isTrue(notelist.notes[0].showNote() === 'hello')
+  console.log('addNoteList test passing')
+})();
+
+(function listNotes() {
+  var notelist = new NoteList()
+  
+  notelist.add('hello')
+  notelist.add('test')
+  console.log(notelist.showNotes())
+  assert.isTrue(notelist.showNotes() == 'hello', 'test')
+  console.log("listNotes test")
+})();
---
+++ note-list-model.js
@@ -0,0 +1,18 @@
+(function(exports) {
+  function NoteList() {
+    this.notes = []
+  }
+
+  NoteList.prototype.add = function(string) {
+    var note = new Note(string)
+    this.notes.push(note)
+  }
+
+  NoteList.prototype.showNotes = function() {
+    for (var i = 0; i < this.notes.length; i++) {
+      return this.notes[i].showNote();
+    }
+  }
+
+  exports.NoteList = NoteLists
+})(this)
